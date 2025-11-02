a8191ce1613b731596eb8ac5b56a2585e275eb19
eslamalawy <eslammo4kala@gmail.com>
2025-11-02 13:29:36 +0200

    Building the App Layout

---
+++ 15-fast-pizza-pizza/src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to={"/"}>Fast React Pizza </Link>
+      <p>hi</p>
+    </header>
+  );
+}
+
+export default Header;
