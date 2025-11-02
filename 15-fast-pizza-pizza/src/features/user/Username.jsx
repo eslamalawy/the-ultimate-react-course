39b46f3d6a8f0ce000a0273767ab6a65ac41dbe0
eslamalawy <eslammo4kala@gmail.com>
2025-11-02 17:46:36 +0200

    Using Flexbox

--- 15-fast-pizza-pizza/src/features/user/Username.jsx
+++ 15-fast-pizza-pizza/src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Eren</div>;
+  return <div className="hidden text-sm font-semibold md:block">Eren</div>;
 }
 
 export default Username;
