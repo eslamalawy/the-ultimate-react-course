871535e381631f2a195cf23cb455cced0673b24e
eslamalawy <eslammo4kala@gmail.com>
2025-11-04 01:10:04 +0200

    Fetching Cabin Data

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,8 @@ import AppLayout from "./ui/AppLayout";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 60 * 1000, //1 Min
+      staleTime: 0, //1 Min
+      // staleTime: 60 * 1000, //1 Min
     },
   },
 });
