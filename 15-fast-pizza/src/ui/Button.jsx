9162e6df7bf7a3309bb4dbe5fb6a496febd7b12a
eslamalawy <eslammo4kala@gmail.com>
2025-11-03 08:25:08 +0200

    Adding Menu Items to the Cart

--- 15-fast-pizza/src/ui/Button.jsx
+++ 15-fast-pizza/src/ui/Button.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const base =
   "inline-block text-sm rounded-full bg-yellow-400 font-semibold tracking-wide text-stone-800 uppercase transition-colors duration-300 hover:cursor-pointer hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed";
-function Button({ children, disabled, to, type }) {
+function Button({ children, disabled, to, type, onClick }) {
   const styles = {
     primary: base + " px-4 py-3 md:px-6 md-py-4",
     small: base + " py-2 px-4 md:px-5 md:py-2.5 text-xs",
@@ -14,6 +14,12 @@ function Button({ children, disabled, to, type }) {
       <Link className={styles[type]} to={to}>
         {children}
       </Link>
+    );
+  if (onClick)
+    return (
+      <button onClick={onClick} className={styles[type]} disabled={disabled}>
+        {children}
+      </button>
     );
 
   return (
