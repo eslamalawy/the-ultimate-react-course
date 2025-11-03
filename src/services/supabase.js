bf78f92cb1170e6a076b2e8f52b1771a41fddf77
eslamalawy <eslammo4kala@gmail.com>
2025-11-04 01:57:02 +0200

    Mutations Deleting a Cabin

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -2,8 +2,8 @@ import { createClient } from "@supabase/supabase-js";
 
 // Create a single supabase client for interacting with your database
 const supabase = createClient(
-  "https://xumtgnqfgzliirnzztyq.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh1bXRnbnFmZ3psaWlybnp6dHlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyMjAxNDcsImV4cCI6MjA3Nzc5NjE0N30.bc90p440Ymx9C4-YKFPNOhA-d3fmNucasHQ236zHuQ0",
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY,
 );
 
 export default supabase;
