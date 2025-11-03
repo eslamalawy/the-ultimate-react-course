e28d9864cc53063a225eda4cefacee403ac0e7d2
eslamalawy <eslammo4kala@gmail.com>
2025-11-03 07:53:31 +0200

    Modeling the User State With Redux Toolkit

--- 15-fast-pizza-pizza/src/main.jsx
+++ 15-fast-pizza-pizza/src/main.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDom from "react-dom/client";
 import App from "./App";
 import "./index.css";
-
+import store from "./store";
+import { Provider } from "react-redux";
 
 ReactDom.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
 );
