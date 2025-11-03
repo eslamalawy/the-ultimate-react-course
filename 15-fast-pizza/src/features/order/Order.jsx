e3c3ceeff1f0cc317de8adbba0bc25d3e19feba2
eslamalawy <eslammo4kala@gmail.com>
2025-11-03 12:24:07 +0200

    using useFetcher to update Data Without Navigation

--- 15-fast-pizza/src/features/order/Order.jsx
+++ 15-fast-pizza/src/features/order/Order.jsx
@@ -9,6 +9,7 @@ import {
 } from "../../utils/helpers";
 import OrderItem from "./OrderItem";
 import { useEffect } from "react";
+import UpdateOrder from "./UpdateOrder";
 function Order() {
   const order = useLoaderData();
   const fetcher = useFetcher();
@@ -84,6 +85,7 @@ function Order() {
           To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
         </p>
       </div>
+      {!priority && <UpdateOrder order={order} />}
     </div>
   );
 }
