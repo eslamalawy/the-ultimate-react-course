14a81c492a571542c870cc0c63c6343b3167db16
eslamalawy <eslammo4kala@gmail.com>
2025-11-04 06:01:00 +0200

    Displaying Toasts (Notifications)

--- 16-the-wild-oasis/src/ui/AppLayout.jsx
+++ 16-the-wild-oasis/src/ui/AppLayout.jsx
@@ -4,7 +4,7 @@ import Header from "./Header";
 import styled from "styled-components";
 
 const Main = styled.main`
-  background-color: var(--color-grey-50);;
+  background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
 `;
 
@@ -14,13 +14,24 @@ const StyledAppLayout = styled.div`
   grid-template-rows: auto 1fr;
   height: 100vh;
 `;
+
+const Container = styled.div`
+  max-width: 120rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+`;
+
 function AppLayout() {
   return (
     <StyledAppLayout>
       <Header />
       <Sidebar />
       <Main>
-        <Outlet />
+        <Container>
+          <Outlet />
+        </Container>
       </Main>
     </StyledAppLayout>
   );
