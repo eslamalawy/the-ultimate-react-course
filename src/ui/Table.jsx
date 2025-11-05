121eb8ac3a7e18b5e14f3d6ae9fe3ea37cf8ef39
eslamalawy <eslammo4kala@gmail.com>
2025-11-05 14:43:35 +0200

    Applying the Render Props Pattern

--- src/ui/Table.jsx
+++ src/ui/Table.jsx
@@ -89,7 +89,10 @@ function Row({ children }) {
     </StyledRow>
   );
 }
-function Body({ children }) {}
+function Body({ data, render }) {
+  if (!data.length) return <Empty>No data to show at the moment</Empty>;
+  return <StyledBody>{data.map(render)}</StyledBody>;
+}
 
 // 4. Add child components as properties to parent component
 Table.Header = Header;
