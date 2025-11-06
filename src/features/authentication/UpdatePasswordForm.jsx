ee563caeb7c00f3c3a1e7f8eb5ad2e99699ea79f
eslamalawy <eslammo4kala@gmail.com>
2025-11-06 08:36:03 +0200

    Updating User Data and Password

--- src/features/authentication/UpdatePasswordForm.jsx
+++ src/features/authentication/UpdatePasswordForm.jsx
@@ -7,10 +7,11 @@ import Input from "../../ui/Input";
 import { useUpdateUser } from "./useUpdateUser";
 
 function UpdatePasswordForm() {
-  const { register, handleSubmit, formState, getValues, reset } = useForm();
-  const { errors } = formState;
-
   const { updateUser, isUpdating } = useUpdateUser();
+  const { register, handleSubmit, formState, reset } = useForm({
+    disabled: isUpdating,
+  });
+  const { errors } = formState;
 
   function onSubmit({ password }) {
     updateUser({ password }, { onSuccess: reset });
@@ -19,14 +20,13 @@ function UpdatePasswordForm() {
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
       <FormRow
-        label="Password (min 8 characters)"
+        label="New Password (min 8 chars)"
         error={errors?.password?.message}
       >
         <Input
           type="password"
           id="password"
           autoComplete="current-password"
-          disabled={isUpdating}
           {...register("password", {
             required: "This field is required",
             minLength: {
@@ -45,16 +45,20 @@ function UpdatePasswordForm() {
           type="password"
           autoComplete="new-password"
           id="passwordConfirm"
-          disabled={isUpdating}
           {...register("passwordConfirm", {
             required: "This field is required",
-            validate: (value) =>
-              getValues().password === value || "Passwords need to match",
+            validate: (value, formValues) =>
+              formValues.password === value || "Passwords need to match",
           })}
         />
       </FormRow>
       <FormRow>
-        <Button onClick={reset} type="reset" variation="secondary">
+        <Button
+          onClick={reset}
+          type="reset"
+          variation="secondary"
+          disabled={isUpdating}
+        >
           Cancel
         </Button>
         <Button disabled={isUpdating}>Update password</Button>
