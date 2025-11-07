4b1461c7d825854aef09ade252a88d56289a9992
eslamalawy <eslammo4kala@gmail.com>
2025-11-07 18:30:16 +0200

    fix minor un used consts - deploy to netlify

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("pejor18707@fantastu.com");
-  const [password, setPassword] = useState("pejor18707@fantastu.com");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { isLoading, login } = useLogin();
   function handleSubmit(e) {
     e.preventDefault();
