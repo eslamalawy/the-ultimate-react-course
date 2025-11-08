461fe472f3656407a4789bd3c24f7d6466eb4cf1
eslamalawy <eslammo4kala@gmail.com>
2025-11-08 22:38:20 +0200

    Adding Page Metadata and Favicon

--- 20-the-wild-oasis-website/app/layout.js
+++ 20-the-wild-oasis-website/app/layout.js
@@ -2,7 +2,11 @@ import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
 import "@/app/_styles/globals.css";
 export const metadata = {
-  title: "The Wild Oasis",
+  title: {
+    template: "%s / The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
+  },
+  description: "Luxurious cabin hotel, located in the heart of Italian Dolomites, surrounded by beautiful mountains and dark forests"
 };
 
 export default function RootLayout({ children }) {
