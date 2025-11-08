7f24cb97aee58ca883175b3d3ac980be661e4879
eslamalawy <eslammo4kala@gmail.com>
2025-11-08 22:45:51 +0200

    Loading and Optimizing Fonts

--- 20-the-wild-oasis-website/app/layout.js
+++ 20-the-wild-oasis-website/app/layout.js
@@ -1,18 +1,26 @@
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
+import { Josefin_Sans } from "next/font/google";
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 import "@/app/_styles/globals.css";
 export const metadata = {
   title: {
     template: "%s / The Wild Oasis",
     default: "Welcome / The Wild Oasis",
   },
-  description: "Luxurious cabin hotel, located in the heart of Italian Dolomites, surrounded by beautiful mountains and dark forests"
+  description:
+    "Luxurious cabin hotel, located in the heart of Italian Dolomites, surrounded by beautiful mountains and dark forests",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+      >
         <header>
           <Logo />
           <Navigation />
