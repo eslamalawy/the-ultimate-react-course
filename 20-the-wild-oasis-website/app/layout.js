ed9ef5946131e121c73f50f2a88ed5f7ff8a3719
eslamalawy <eslammo4kala@gmail.com>
2025-11-08 23:12:53 +0200

    Improving the Navigation and Root Layout + improve fonts with antialiased

--- 20-the-wild-oasis-website/app/layout.js
+++ 20-the-wild-oasis-website/app/layout.js
@@ -6,6 +6,7 @@ const josefin = Josefin_Sans({
   display: "swap",
 });
 import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
 export const metadata = {
   title: {
     template: "%s / The Wild Oasis",
@@ -19,14 +20,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefin.className} flex min-h-screen flex-col bg-primary-950 text-primary-100 antialiased`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="mx-auto max-w-7xl">{children}</main>
+        </div>
       </body>
     </html>
   );
