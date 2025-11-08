1004c912afb25f057874e227e0e5e4419c2e3ddd
eslamalawy <eslammo4kala@gmail.com>
2025-11-08 16:41:33 +0200

    Creating a Layout

--- 20-the-wild-oasis-website/app/page.js
+++ 20-the-wild-oasis-website/app/page.js
@@ -1,10 +1,8 @@
 import Link from "next/link";
-import Navigation from "./components/Navigation";
 
 export default function Page() {
   return (
     <div>
-      <Navigation />
       <h1>The Wild Oasis. Welcome to here!</h1>
       <Link href="/cabins">Explore luxury cabins</Link>
     </div>
