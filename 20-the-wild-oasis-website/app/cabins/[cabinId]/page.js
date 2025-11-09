745503b766fcb9620cc098fa9201acbcd7baf9fd
eslamalawy <eslammo4kala@gmail.com>
2025-11-09 05:11:47 +0200

    Error Handling Not Found Errors

--- 20-the-wild-oasis-website/app/cabins/[cabinId]/page.js
+++ 20-the-wild-oasis-website/app/cabins/[cabinId]/page.js
@@ -39,7 +39,7 @@ export default async function Page({ params }) {
             <li className="flex items-center gap-3">
               <UsersIcon className="h-5 w-5 text-primary-600" />
               <span className="text-lg">
-                For up to <span className="font-bold">{max.Capacity}</span>{" "}
+                For up to <span className="font-bold">{maxCapacity}</span>{" "}
                 guests
               </span>
             </li>
