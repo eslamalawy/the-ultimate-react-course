55cedbd5158e041dc9a6d49f1e126ccb98aba4cd
eslamalawy <eslammo4kala@gmail.com>
2025-11-11 00:43:14 +0200

    Creating a New Guest on First Sign In

--- 20-the-wild-oasis-website/app/_lib/auth.js
+++ 20-the-wild-oasis-website/app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
 
 const authConfig = {
   providers: [
@@ -13,6 +14,25 @@ const authConfig = {
       // auth here is the session - request is sent by the middleware
       // !! is used to convert this to boolean instead of if(auth.user) return true
       return !!auth?.user;
+    },
+    async signIn({ user, account, profile }) {
+      // this callback actually runs before the actual signup process happens so that means we can perform all kinds of operations here that are associated with the signing in process
+      // works as middleware it happens after the user has put in their credentials but before they actually really logged into the application
+      try {
+        const existingGuest = await getGuest(user.email);
+        if (!existingGuest)
+          await createGuest({ email: user.email, fullName: user.name });
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    async session({ session, user }) {
+      // this callback is gonna be called the session callback, and it runs after the sign in callback - and also each time that the session is checked out
+      const guest = await getGuest(session.user.email);
+      session.user.guestId = guest.id;
+      return session;
     },
   },
   pages: {
