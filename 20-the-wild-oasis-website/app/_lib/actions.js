4b702e534fdc78190bb72e484758c9482f5f3c20
eslamalawy <eslammo4kala@gmail.com>
2025-11-11 00:22:32 +0200

    Building a Custom Sign Out Button Even it is client component - with server action  signOutAction

--- 20-the-wild-oasis-website/app/_lib/actions.js
+++ 20-the-wild-oasis-website/app/_lib/actions.js
@@ -1,9 +1,13 @@
 "use server";
 
-import { signIn } from "./auth";
+import { signIn, signOut } from "./auth";
 
 export async function signInAction() {
   // if you have many providers  so you have to get provider name from the api route http://localhost:3000/api/auth/providers
   // and loop over it to get your specific provider
   await signIn("google", { redirectTo: "/account" });
+}
+
+export async function signOutAction() {
+  await signOut({ redirectTo: "/" });
 }
